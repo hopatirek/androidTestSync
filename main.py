d62bf03f8fb811fe50651c2c7cfbd7bd8bcb606c
Atirek Roy <atirekroy@goevive.com>
2018-11-23 14:51:22 +0530

    made something that can copy key inputs

--- main.py
+++ main.py
@@ -2,20 +2,18 @@
 from subprocess import Popen,PIPE,STDOUT
 from time import time
 import bs4
-import  sys
+from androidKeyMaps import KEY_MAP
 STR_OPENING_SQUARE_BRACKET = "["
 STR_CLOSING_SQUARE_BRACKET = "]"
+listOfPrevValues = []
+
 def main():
     STR_POS_Y = "ABS_MT_POSITION_X"
     STR_POS_X = "ABS_MT_POSITION_Y"
-    statusOfTouch = 0
-    flag = 0
     listOfPosWithTime = []
-    REF_TIME_DIFF = 1542649043
 
     streamFromAdb = Popen(["adb", "shell", "getevent", "-lt"],stdout=PIPE,stderr=STDOUT,bufsize=0)
 
-    timeOfRecordingOfEvent=0
 
     for line in iter(streamFromAdb.stdout.readline, b''):
           line = line.strip().decode('utf-8')
@@ -23,18 +21,16 @@ def main():
           #print(line)
 
           if line.find(STR_OPENING_SQUARE_BRACKET)> -1:
-              timeDiff = int(time() - timeParsed)
-              if flag == 0:
-                  REF_TIME_DIFF = time() - timeParsed # defined ref time difference
-                  flag = 1
-
-              if timeDiff!=REF_TIME_DIFF and statusOfTouch == 0 and len(listOfPosWithTime)>0:
+              if len(listOfPosWithTime)>0:
+                  currentTime = time()
+                  if (len(listOfPosWithTime) == 1):
+                    lastTime = time() # lasttime is initialised here
+                    initialisePreviousValue()
+                  if (currentTime - lastTime > 0.5):
                     getEventType(listOfPosWithTime)
-                    timeOfRecordingOfEvent = time()
                     listOfPosWithTime = []
-                    statusOfTouch=1
-          if time()-timeOfRecordingOfEvent > 0.5:
-              statusOfTouch = 0
+
+
 
 
           if line != '':
@@ -47,6 +43,15 @@ def main():
 
 
 
+def initialisePreviousValue():
+    Popen(["adb", "shell", "uiautomator", "dump"])
+    outputXML = Popen(["adb", "shell", "cat", "/sdcard/window_dump.xml"],stdout=PIPE).communicate()[0]
+    soup = bs4.BeautifulSoup(outputXML,'xml')
+    editTextList = soup.findAll("node", {"class": "android.widget.EditText"})
+    i = 0
+    for editText in editTextList:
+        listOfPrevValues.append(editText['text'])
+        i=i+1
 
 def parseTime(line):
 
@@ -66,29 +71,85 @@ def parsePosition(line, STR_POS):
     return pos
 
 def getEventType(listOfPosWithTime):
-    lenOfList = len(listOfPosWithTime)
+    lastIndexOfList = len(listOfPosWithTime)-1
+
     endTimeOfEvent = listOfPosWithTime[0][0]
-    startTimeOfEvent = listOfPosWithTime[lenOfList-1][0]
-
+    startTimeOfEvent = listOfPosWithTime[lastIndexOfList][0]
     intervalOfEvent = endTimeOfEvent - startTimeOfEvent
-    print(intervalOfEvent,listOfPosWithTime)
-
-    getTextEntered()
-    if(intervalOfEvent>0.07):
-        print("Long touch")
-    else:
-        print("Short touch")
 
 
+    firstWidth = str(listOfPosWithTime[lastIndexOfList][1])
+    lastWidth = str(listOfPosWithTime[0][1])
+    diffBetweenWidth = abs(int(lastWidth) - int(firstWidth))
 
-def getTextEntered():
+    firstHeight = str(listOfPosWithTime[lastIndexOfList][2])
+    lastHeight = str(listOfPosWithTime[0][2])
+    diffBetHeight = abs(int(lastHeight) - int(firstHeight))
 
+    if(textNotEntered()):
+        if intervalOfEvent > 0.1:
+
+            if diffBetHeight < 8 or diffBetweenWidth < 8:
+                Popen(["adb", "shell", "input", "touchscreen", "swipe",
+                       firstWidth,firstHeight,
+                       firstWidth,firstHeight,
+                       '1000'])
+                print("Long Press")
+            else:
+                Popen(["adb", "shell", "input", "touchscreen", "swipe",
+                       firstWidth, firstHeight,
+                       lastWidth, firstHeight,
+                       '1000'])
+                print("Swipe")
+
+        else:
+            Popen(["adb", "shell", "input", "tap",
+                   firstWidth, firstHeight])
+            print("Click")
+
+
+
+
+
+def textNotEntered():
     Popen(["adb", "shell", "uiautomator", "dump"])
     outputXML = Popen(["adb", "shell", "cat", "/sdcard/window_dump.xml"],stdout=PIPE).communicate()[0]
     soup = bs4.BeautifulSoup(outputXML,'xml')
     editTextList = soup.findAll("node", {"class": "android.widget.EditText"})
+    i = 0
     for editText in editTextList:
+        i = i + 1
         print(editText['text'])
+        if len(listOfPrevValues)!=len(editText):
+            compareAndType(editText['text'],listOfPrevValues[i])
+            return False
+
+
+    return True
+
+
+
+
+
+
+
+def compareAndType(editText,prevEditText):
+    lenOfEditText = len(editText)
+    lenOfPrevEditText = len(prevEditText)
+    if lenOfEditText < lenOfPrevEditText:
+        for i in range(lenOfEditText-1,lenOfPrevEditText):
+            Popen(["adb","shell","input","keyevent",KEY_MAP['BACKSLASH']])
+    else:
+        for i in range(lenOfPrevEditText-1,lenOfEditText):
+            inputText = prevEditText[i]
+            print(inputText)
+            Popen(["adb", "shell", "input", "text",inputText])
+
+
+
+
+
+
 
     #print(soup.prettify())
 
